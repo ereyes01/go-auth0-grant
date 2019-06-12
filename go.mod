97d56601c1c9d64db910bf68498304d7705d2444
Eddy Reyes <ereyes01@users.noreply.github.com>
2019-06-11 19:28:18 -0500

    support go modules, ditch ginkgo/gomega

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ereyes01/go-auth0-grant
+
+go 1.12
+
+require (
+	github.com/google/go-cmp v0.3.0
+	github.com/pkg/errors v0.8.1
+)
--- grant_test.go
+++ grant_test.go
@@ -7,45 +7,89 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/format"
+	"github.com/google/go-cmp/cmp"
 )
 
-var testGrantResponse = `{"access_token":"open-sesame","scope":"u-cant-touch-dis","expires_in":1,"token_type":"Bearer"}`
+var (
+	expectedGrant = clientCredsGrant{
+		AccessToken: "open-sesame",
+		Scope:       "u-cant-touch-dis",
+		TokenType:   "Bearer",
+		ExpiresIn:   time.Second,
+	}
 
-var testGrant = clientCredsGrant{
-	AccessToken: "open-sesame",
-	Scope:       "u-cant-touch-dis",
-	TokenType:   "Bearer",
-	ExpiresIn:   time.Second,
+	testCredRequest = CredentialsRequest{
+		ClientID:     "joe-blow-id",
+		ClientSecret: "joe-blow-secret",
+		Audience:     "https://api.blowcorp.co/",
+		GrantType:    CLIENT_CREDS_GRANT_TYPE,
+	}
+)
+
+const (
+	testGrantResponse = `{"access_token":"open-sesame","scope":"u-cant-touch-dis","expires_in":1,"token_type":"Bearer"}`
+	tokenPath         = "/oauth/token"
+)
+
+type testGrantServer struct {
+	server *httptest.Server
+	ncalls int
+	t      *testing.T
 }
 
-var testCredRequest = CredentialsRequest{
-	ClientID:     "joe-blow-id",
-	ClientSecret: "joe-blow-secret",
-	Audience:     "https://api.blowcorp.co/",
-	GrantType:    CLIENT_CREDS_GRANT_TYPE,
+func newTestGrantServer(t *testing.T, ncalls int) *testGrantServer {
+	t.Helper()
+
+	s := &testGrantServer{t: t, ncalls: ncalls}
+	s.server = httptest.NewServer(http.HandlerFunc(s.handler))
+	return s
 }
 
-func testGetAccessToken() (http.HandlerFunc, *int) {
-	var ncalls int
+func (s *testGrantServer) Close() {
+	s.t.Helper()
 
-	return func(w http.ResponseWriter, r *http.Request) {
-		Expect(r.URL.Path).To(Equal("/oauth/token"))
-		Expect(r.Method).To(Equal("POST"))
-		Expect(r.Header.Get("Content-Type")).To(Equal("application/json"))
+	if s.ncalls != 0 {
+		s.t.Fatalf("wrong # calls got: %d expected %d", s.ncalls, 0)
+	}
 
-		var request CredentialsRequest
-		Expect(json.NewDecoder(r.Body).Decode(&request)).To(Succeed())
-		Expect(r.Body.Close()).To(Succeed())
-		Expect(request).To(Equal(testCredRequest))
+	s.server.Close()
+}
 
-		_, err := w.Write([]byte(testGrantResponse))
-		Expect(err).To(BeNil())
+func (s *testGrantServer) TokenURL() string {
+	s.t.Helper()
 
-		ncalls++
-	}, &ncalls
+	return s.server.URL + tokenPath
+}
+
+func (s *testGrantServer) handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != tokenPath {
+		s.t.Fatalf("url path got: %s expected: %s", r.URL.Path, tokenPath)
+	}
+	if r.Method != "POST" {
+		s.t.Fatalf("http method got: %s expected: %s", r.Method, "POST")
+	}
+	mimeType := r.Header.Get("Content-Type")
+	if mimeType != "application/json" {
+		s.t.Fatalf("mime type got: %s expected: %s", mimeType, "application/json")
+	}
+
+	var request CredentialsRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		s.t.Fatal("json decode body:", err)
+	}
+	if err := r.Body.Close(); err != nil {
+		s.t.Fatal("close request body:", err)
+	}
+	if !cmp.Equal(request, testCredRequest) {
+		s.t.Fatalf("cred request expected: %+v got: %+v", request, testCredRequest)
+	}
+
+	if _, err := w.Write([]byte(testGrantResponse)); err != nil {
+		s.t.Fatal("write grant response:", err)
+	}
+
+	s.ncalls--
 }
 
 func testNewGrant(tokenURL string, credRequest *CredentialsRequest, nowFn func() time.Time) Grant {
@@ -60,74 +104,76 @@ func testNowFn(offset int64) func() time.Time {
 	}
 }
 
-var _ = Describe("Credentials Grant API", func() {
-	var (
-		tokenServer *httptest.Server
-		nTokenCalls *int
-		tokenURL    string
-	)
+func TestGrantAPI(t *testing.T) {
+	t.Run("Retrieves the access token for the first time", func(t *testing.T) {
+		server := newTestGrantServer(t, 1)
+		defer server.Close()
 
-	BeforeEach(func() {
-		var h http.HandlerFunc
-		h, nTokenCalls = testGetAccessToken()
-
-		tokenServer = httptest.NewServer(h)
-		tokenURL = tokenServer.URL + "/oauth/token"
-	})
-
-	AfterEach(func() {
-		tokenServer.Close()
-		tokenServer = nil
-		nTokenCalls = nil
-		tokenURL = ""
-	})
-
-	It("Retrieves the access token for the first time", func() {
-		grant := testNewGrant(tokenURL, &testCredRequest, testNowFn(1))
+		grant := testNewGrant(server.TokenURL(), &testCredRequest, testNowFn(1))
 
 		token, err := grant.GetAccessToken()
-		Expect(err).To(BeNil())
-		Expect(token).To(Equal(testGrant.AccessToken))
-		Expect(*nTokenCalls).To(Equal(1))
+		if err != nil {
+			t.Fatal("get access token:", err)
+		}
+		if token != expectedGrant.AccessToken {
+			t.Fatalf("wrong access token got: %s expected: %s", token, expectedGrant.AccessToken)
+		}
 
 		req, ok := grant.(*grantRequest)
-		Expect(ok).To(BeTrue())
-		Expect(*req.grant).To(Equal(testGrant))
+		if !ok {
+			t.Fatal("cast to *grantRequest")
+		}
+		if !cmp.Equal(*req.grant, expectedGrant) {
+			t.Fatalf("wrong grant got: %+v expected: %+v", *req.grant, expectedGrant)
+		}
 	})
 
-	It("Renews and retrieves an access token when the previous one has expired", func() {
-		grant := testNewGrant(tokenURL, &testCredRequest, testNowFn(3))
-		req, ok := grant.(*grantRequest)
-		Expect(ok).To(BeTrue())
+	t.Run("Renews and retrieves an access token when the previous one has expired", func(t *testing.T) {
+		server := newTestGrantServer(t, 1)
+		defer server.Close()
 
-		req.grant = &testGrant
+		grant := testNewGrant(server.TokenURL(), &testCredRequest, testNowFn(3))
+		req, ok := grant.(*grantRequest)
+		if !ok {
+			t.Fatal("cast to *grantRequest")
+		}
+
+		req.grant = &expectedGrant
 		req.issuedAt = time.Unix(1, 0)
 
 		token, err := grant.GetAccessToken()
-		Expect(err).To(BeNil())
-		Expect(token).To(Equal(testGrant.AccessToken))
-		Expect(*nTokenCalls).To(Equal(1))
+		if err != nil {
+			t.Fatal("get access token:", err)
+		}
+		if token != expectedGrant.AccessToken {
+			t.Fatalf("wrong access token got: %s expected: %s", token, expectedGrant.AccessToken)
+		}
 
-		Expect(req.issuedAt).To(Equal(time.Unix(3, 0)))
+		expectedTime := time.Unix(3, 0)
+		if !req.issuedAt.Equal(expectedTime) {
+			t.Fatalf("wrong issue time got: %s, expected: %s", req.issuedAt.Format(time.RFC3339), expectedTime.Format(time.RFC3339))
+		}
 	})
 
-	It("Retrieves the cached access token if it isn't expired yet", func() {
-		grant := testNewGrant(tokenURL, &testCredRequest, testNowFn(1))
-		req, ok := grant.(*grantRequest)
-		Expect(ok).To(BeTrue())
+	t.Run("Retrieves the cached access token if it isn't expired yet", func(t *testing.T) {
+		server := newTestGrantServer(t, 0) // <-- server shouldn't get called!
+		defer server.Close()
 
-		req.grant = &testGrant
+		grant := testNewGrant(server.TokenURL(), &testCredRequest, testNowFn(1))
+		req, ok := grant.(*grantRequest)
+		if !ok {
+			t.Fatal("cast to *grantRequest")
+		}
+
+		req.grant = &expectedGrant
 		req.issuedAt = time.Unix(1, 0)
 
 		token, err := grant.GetAccessToken()
-		Expect(err).To(BeNil())
-		Expect(token).To(Equal(testGrant.AccessToken))
-		Expect(*nTokenCalls).To(BeZero())
+		if err != nil {
+			t.Fatal("get access token:", err)
+		}
+		if token != expectedGrant.AccessToken {
+			t.Fatalf("wrong access token got: %s expected: %s", token, expectedGrant.AccessToken)
+		}
 	})
-})
-
-func TestAuth(t *testing.T) {
-	RegisterFailHandler(Fail)
-	format.UseStringerRepresentation = true // don't print spammy time structs
-	RunSpecs(t, "auth0grant package")
 }
